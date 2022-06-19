1f846c90b2523c5d24628caf9cd79c44534c5c86
HansPuente <hpuentej@uni.pe>
2022-06-19 18:40:31 -0500

    finalizado hasta minuto 38 fazt react localstorage

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
 import "./App.css";
 import React from "react";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import {TaskCreator} from "./components/TaskCreator";
 
 function App() {
   const [tasksItems, setTasksItems] = useState([
-    {name: "Task 1", isDone: false},
-    {name: "Task 2", isDone: false},
-    {name: "Task 3", isDone: false},
+    {name: "Example's task", isDone: false},
   ]);
 
   const listTasks = tasksItems.map((task, index) => {
     return (
-      <tr key={index}>
-        {/* <input type="checkbox" checked={task.isDone}/>  */}
+      <td key={task.name}>
         {/* <input type="checkbox" /> */}
         {/* <span>{task.name}</span> */}
         {task.name}
-      </tr>
+      </td> // td means table data, th means table header and tr means table row
     );
   })
 
+  function createNewTask(taskName){    
+    !tasksItems.find(task => task.name === taskName || taskName==='')
+      ? setTasksItems(tasksItems.concat({name: taskName, isDone: false}))
+      : alert("Task already exists or is empty");
+    // setTasksItems([...tasksItems, {name: "Task 4", isDone: false}]);
+    
+  }
+
+  useEffect(()=>{
+    const data = localStorage.getItem('tasksItems')
+    if (data) {
+      setTasksItems(JSON.parse(data))
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('tasksItems', JSON.stringify(tasksItems));
+  }, [tasksItems]);
+
   return (
     <div className="App">
-      <TaskCreator/>
-      <table>
-        <thead>
-          <tr>
+      <TaskCreator
+        createNewTask={createNewTask}
+      />
+      <table className='table'>
+        <thead> 
+          <tr> 
             <th>Tasks</th>
           </tr>
         </thead>
         <tbody>
-          {/* {listTasks} */}
+          <tr className="activities">
+            {listTasks}
+          </tr>
         </tbody>
       </table>
     </div>
   );
 }
+
 export default App;
--- src/components/TaskCreator.js
+++ src/components/TaskCreator.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 
-export const TaskCreator = () => {
-  const [newTaskName, setNewTaskName] = useState('');
-  const handleSubmit = (e) =>{
+export const TaskCreator = ({ createNewTask }) => {
+  const [newTaskName, setNewTaskName] = useState("");
+  const handleSubmit = (e) => {
     e.preventDefault(); // prevent default behavior of the form and not reload the page
-    localStorage.setItem(newTaskName, 'New Task')    
-    setNewTaskName('');
-  }
+    createNewTask(newTaskName);
+    setNewTaskName("");
+  }; 
   return (
     <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter a new activity"
-          value={newTaskName}
-          onChange={(e) => setNewTaskName(e.target.value)}
-        />
-        <button>
-          Add activity
-        </button>
-      </form>
-  )
-}
+      <input
+        type="text"
+        placeholder="Enter a new activity"
+        value={newTaskName}
+        onChange={(e) => setNewTaskName(e.target.value)}
+      />
+      <button>Add activity</button>
+    </form>
+  );
+};
